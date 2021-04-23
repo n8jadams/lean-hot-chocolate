5cf55e386b9a209540537dfc8a47e6be987760f1
maxwell clemens <maxwell-clemens@pluralsight.com>
2021-04-23 16:03:14 -0700

    create continue voting

--- src/ContinueVoting.tsx
+++ src/ContinueVoting.tsx
@@ -1,9 +1,57 @@
 import React from 'react'
 import { EndMeetingButton } from './EndMeetingButton';
-import { Props } from './machine-types-consts';
+import { ContinueVote, Props } from './machine-types-consts';
+import { Button, Card, CardActions, CardContent, Typography } from "@material-ui/core";
+import { getUserById } from './utils';
+import { send } from './send';
 
-export function ContinueVoting({ context }: Props): React.ReactElement<Props> {
-	return <>
-		<EndMeetingButton />
+export function ContinueVoting({ context, currentUserId }: Props): React.ReactElement<Props> {
+	const winningTopicId = context.topicVoteResults[0]
+	const topic = context.topics.find(topic => topic.id === winningTopicId)
+	if (!topic) {
+		return <></>
+	}
+  
+  return <>
+  <Typography component="h2">Continue Discussing?</Typography>
+			<Card key={topic.id} variant="outlined">
+				<CardContent>
+					<Typography>Number of votes: {topic.votesCastFor.length}</Typography>
+					<Typography variant="h5" component="h2">
+						{topic.name}
+					</Typography>
+					<Typography color="textSecondary">
+						Submitted by:{" "}
+						{getUserById(topic.createdByUserId, context.users)?.username ??
+							""}
+					</Typography>
+				</CardContent>
+			</Card>
+      <div>
+      <Button onClick={() => {
+        if(context.continueVotes[ContinueVote.YES].includes(currentUserId??'')){
+          send({type: 'RETRACT_CONTINUE_VOTE'})
+        } else {
+          send({type: 'PLACE_CONTINUE_VOTE', value: ContinueVote.YES})
+        }
+      }} >👍</Button>
+      <Typography component="p">Current Votes: {context.continueVotes[ContinueVote.YES].length}</Typography>
+      </div>
+      <div>
+      <Button onClick={() => {
+        if(context.continueVotes[ContinueVote.NO].includes(currentUserId??'')){
+          send({type: 'RETRACT_CONTINUE_VOTE'})
+        } else {
+          send({type: 'PLACE_CONTINUE_VOTE', value: ContinueVote.NO})
+        }
+      }}>👎</Button>
+      <Typography component="p">Current Votes: {context.continueVotes[ContinueVote.NO].length}</Typography>
+      </div>
+      <Typography component="p">Abstain Votes: {context.users.length - (context.continueVotes[ContinueVote.NO].length + context.continueVotes[ContinueVote.YES].length)}</Typography>
+		<Button color="primary"
+		variant="contained" onClick={() => {
+			send({type: 'END_CONTINUE_VOTING'})
+		}}>End Voting</Button>
+    <EndMeetingButton />
 	</>
 }
